58741bdc566076dde3c4206b93d37cdca7614092
konkarah <KONKARAHDEH@GMAIL.COM>
2023-09-28 12:55:05 +0300

    almost all additions

--- server.js
+++ server.js
@@ -3,53 +3,116 @@ const app = express()
 const nodemailer = require('nodemailer');
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Order = require('./models/Orders')
 const port = 3000
 
 app.use(cors())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
+mongoose.set("strictQuery", false);
+
+mongoose.connect('mongodb+srv://deh:1234@cluster0.lqv4l5b.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser: true,useUnifiedTopology: true})
+        .then(()=> console.log("Database connected"))
+        .catch(err => console.log(err))
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-app.post('/test',mymailer, (req,res)=> {
+app.post('/test',async(req,res)=> {
     const account = req.body.account
     const amount = req.body.amount
-    const mail = req.body.mail
+    
 
-    console.log(account, amount, mail)
+    console.log(account, amount)
 
-    mymailer(mail.tostring(), account)
+    const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    requireTLS: true,
+    auth: {
+        // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+        user: "itsthindi@gmail.com",
+        pass: "ynbgzxupqcikhtig",
+    },
+    });
+
+    // async..await is not allowed in global scope, must use a wrapper
+    async function main() {
+    // send mail with defined transport object
+    const info = await transporter.sendMail({
+        from: 'itsthindi@gmail.com', // sender address
+        to: req.body.mail, // list of receivers
+        subject: "Account Request", // Subject line
+        text: "Request for a"+account+" account provision for " +req.body.mail+".", // plain text body
+        html: `<b>Request for a `+account+` account provision for ` +req.body.mail+`.</b>`, // html body
+    });
+
+    console.log("Message sent: %s", info.messageId);
+    
+    }
+
+    main().catch(console.error);
+
+    const order = new Order({
+        custname: req.body.mail,
+        account: account,
+        amount: amount
+      })
+    
+      try{
+        const cafeposted = await order.save()
+        console.log(cafeposted)
+      }catch(err){
+        console.log(err)
+      }
 
     res.send("Good")
+})
+
+app.get('/myadmin', async(req,res)=> {
+    const filter = {};
+    const all = await Order.find(filter);
+    //console.log(all)
+    res.json(all)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-function mymailer(themail,account){
-    const transporter = nodemailer.createTransport({
-        host: "smtp.gmail.com",
-        port: 587,
-        secure: false,
-        requireTLS: true,
-        auth: {
-          // TODO: replace `user` and `pass` values from <https://forwardemail.net>
-          user: "itsthindi@gmail.com",
-          pass: "",
-        },
-      });
-      
-      // async..await is not allowed in global scope, must use a wrapper
-      async function main() {
-        // send mail with defined transport object
-        const info = await transporter.sendMail({
-          from: 'itsthindi@gmail.com', // sender address
-          to: themail, // list of receivers
-          subject: "Hello ✔", // Subject line
-          text: "Hello world?", // plain text body
-          html: "<b>Hello world?</b>", // html body
-        });
-    }      
+async function mymailer(themail,account){
+    const mail = themail
+const nodemailer = require("nodemailer");
+
+const transporter = nodemailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  requireTLS: true,
+  auth: {
+    // TODO: replace `user` and `pass` values from <https://forwardemail.net>
+    user: "itsthindi@gmail.com",
+    pass: "ynbgzxupqcikhtig",
+  },
+});
+
+// async..await is not allowed in global scope, must use a wrapper
+async function main() {
+  // send mail with defined transport object
+  const info = await transporter.sendMail({
+    from: 'itsthindi@gmail.com', // sender address
+    to: "${mail}", // list of receivers
+    subject: "Hello ✔", // Subject line
+    text: "Hello world?", // plain text body
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  console.log("Message sent: %s", info.messageId);
+  
+}
+
+main().catch(console.error);
 }
